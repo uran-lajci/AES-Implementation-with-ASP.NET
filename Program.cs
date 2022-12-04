8e51b0bd163be44c2609a8e2f306460719fc684c
elvirmisini <58117020+elvirmisini@users.noreply.github.com>
2022-12-04 11:19:11 +0100

    Decryption

--- Program.cs
+++ Program.cs
@@ -59,3 +59,36 @@ namespace AesEncryption {
                 return Convert.ToBase64String(encryptedData);
             }
         }
+         private static string DecryptDataWithAes(string cipherText, string keyBase64, string vectorBase64) {
+
+            try {
+                using(Aes aesAlgorithm = Aes.Create()) {
+                    aesAlgorithm.Key = Convert.FromBase64String(keyBase64);
+                    aesAlgorithm.IV = Convert.FromBase64String(vectorBase64);
+
+                    Console.WriteLine($"Aes Cipher Mode : {aesAlgorithm.Mode}");
+                    Console.WriteLine($"Aes Padding Mode: {aesAlgorithm.Padding}");
+                    Console.WriteLine($"Aes Key Size : {aesAlgorithm.KeySize}");
+                    Console.WriteLine($"Aes Block Size : {aesAlgorithm.BlockSize}");
+
+                    // Create decryptor object
+                    ICryptoTransform decryptor = aesAlgorithm.CreateDecryptor();
+
+                    byte[] cipher = Convert.FromBase64String(cipherText);
+
+                    //Decryption will be done in a memory stream through a CryptoStream object
+                    using(MemoryStream ms = new MemoryStream(cipher)) {
+                        using(CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read)) {
+                            using(StreamReader sr = new StreamReader(cs)) {
+                                return sr.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            } catch (Exception ex) {
+                Console.WriteLine("Wrong input!" + ex.Message);
+                return "";
+            }
+
+        }
+
