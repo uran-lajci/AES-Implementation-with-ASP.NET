771f24957992f275a87584e767e85c085111ea71
elvirmisini <58117020+elvirmisini@users.noreply.github.com>
2022-12-04 11:17:55 +0100

    Encrypt

--- Program.cs
+++ Program.cs
@@ -30,3 +30,32 @@ namespace AesEncryption {
                 }
             }
         }
+  private static string EncryptDataWithAes(string plainText, out string keyBase64, out string vectorBase64) {
+            using(Aes aesAlgorithm = Aes.Create()) {
+                Console.WriteLine($"Aes Cipher Mode : {aesAlgorithm.Mode}");
+                Console.WriteLine($"Aes Padding Mode: {aesAlgorithm.Padding}");
+                Console.WriteLine($"Aes Key Size : {aesAlgorithm.KeySize}");
+                Console.WriteLine($"Aes Block Size : {aesAlgorithm.BlockSize}");
+
+                //set the parameters with out keyword
+                keyBase64 = Convert.ToBase64String(aesAlgorithm.Key);
+                vectorBase64 = Convert.ToBase64String(aesAlgorithm.IV);
+
+                // Create encryptor object
+                ICryptoTransform encryptor = aesAlgorithm.CreateEncryptor();
+
+                byte[] encryptedData;
+
+                //Encryption will be done in a memory stream through a CryptoStream object
+                using(MemoryStream ms = new MemoryStream()) {
+                    using(CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write)) {
+                        using(StreamWriter sw = new StreamWriter(cs)) {
+                            sw.Write(plainText);
+                        }
+                        encryptedData = ms.ToArray();
+                    }
+                }
+
+                return Convert.ToBase64String(encryptedData);
+            }
+        }
