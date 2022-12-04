da051abeee560af38a011fd130a776fdfad3a5ab
uran-lajci <117693854+uran-lajci@users.noreply.github.com>
2022-12-04 13:04:48 +0100

    added the validation methods for decryption

--- Program.cs
+++ Program.cs
@@ -109,3 +109,74 @@ namespace AesEncryption {
             Console.WriteLine("--------------------------------------------------------------");
             return plainText;
         }
+private static void printEncryptionData(string cipherText, string keyBase64, string vectorBase64) {
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Here is the cipher text:");
+            Console.WriteLine(cipherText);
+
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Here is the Aes key in Base64:");
+            Console.WriteLine(keyBase64);
+
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Here is the Aes IV in Base64:");
+            Console.WriteLine(vectorBase64);
+        }
+
+        // Decryption validation and print methods
+
+        private static string readCiphertext() {
+            Console.WriteLine("Please enter the text that you want to decrypt:");
+            string cipherText = "";
+            bool isCipherTextValid = true;
+            while (isCipherTextValid) {
+                cipherText = Console.ReadLine();
+                if (cipherText.Length == 0) {
+                    Console.WriteLine("Invalid ciphertext, it should not be empty");
+                } else {
+                    isCipherTextValid = false;
+                }
+            }
+            Console.WriteLine("--------------------------------------------------------------");
+            return cipherText;
+        }
+
+        private static string readAesKey() {
+            Console.WriteLine("Provide the Aes Key:");
+            string keyBase64 = "";
+            bool isKeyBase64Valid = true;
+            while (isKeyBase64Valid) {
+                keyBase64 = Console.ReadLine();
+                if (keyBase64.Length != 44) {
+                    Console.WriteLine("Invalid Aes Key, it should be 44 characters long");
+                } else {
+                    isKeyBase64Valid = false;
+                }
+            }
+            Console.WriteLine("--------------------------------------------------------------");
+            return keyBase64;
+        }
+
+        private static string readInitializationVector() {
+            Console.WriteLine("Provide the initialization vector:");
+            string vectorBase64 = "";
+            bool isVectorBase64Valid = true;
+            while (isVectorBase64Valid) {
+                vectorBase64 = Console.ReadLine();
+                if (vectorBase64.Length != 24) {
+                    Console.WriteLine("Invalid initialization vector, it should be 24 characters long");
+                } else {
+                    isVectorBase64Valid = false;
+                }
+            }
+            Console.WriteLine("--------------------------------------------------------------");
+            return vectorBase64;
+        }
+
+        private static void printDecryptionData(string plainText) {
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Here is the decrypted data:");
+            Console.WriteLine(plainText);
+        }
+    }
+}
